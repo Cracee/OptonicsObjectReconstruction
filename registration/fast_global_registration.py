69dfc7bd618a079be938cb6bc8c6c06e2368a685
Gregor <Gregormax.Stief@web.de>
2022-12-08 16:20:45 +0100

    implemented the fast global registration by open3d tutorial

---
+++ registration/fast_global_registration.py
@@ -0,0 +1,127 @@
+import open3d as o3d
+import copy
+import numpy
+
+from util_functions import read_pcd_file, save_pcd_file
+
+
+def draw_registration_result(source, target, transformation):
+    source_temp = copy.deepcopy(source)
+    target_temp = copy.deepcopy(target)
+    source_temp.paint_uniform_color([1, 0.706, 0])
+    target_temp.paint_uniform_color([0, 0.651, 0.929])
+    source_temp.transform(transformation)
+    o3d.visualization.draw_geometries(
+        [source_temp, target_temp],
+        zoom=0.4459,
+        front=[0.9288, -0.2951, -0.2242],
+        lookat=[1.6784, 2.0612, 1.4451],
+        up=[-0.3402, -0.9189, -0.1996],
+    )
+
+
+def preprocess_point_cloud(pcd, voxel_size):
+    print(":: Downsample with a voxel size %.3f." % voxel_size)
+    pcd_down = pcd.voxel_down_sample(voxel_size)
+
+    radius_normal = voxel_size * 2
+    print(":: Estimate normal with search radius %.3f." % radius_normal)
+    pcd_down.estimate_normals(
+        o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=30)
+    )
+
+    radius_feature = voxel_size * 5
+    print(":: Compute FPFH feature with search radius %.3f." % radius_feature)
+    pcd_fpfh = o3d.pipelines.registration.compute_fpfh_feature(
+        pcd_down,
+        o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100),
+    )
+    return pcd_down, pcd_fpfh
+
+
+def prepare_dataset(voxel_size):
+    print(":: Load two point clouds and disturb initial pose.")
+    source = read_pcd_file("data/14_ramsphere/cluster_0.pcd", visualize=False)
+    target = read_pcd_file("data/14_ramsphere/cluster_2.pcd", visualize=False)
+    trans_init = numpy.asarray(
+        [
+            [0.0, 0.0, 1.0, 0.0],
+            [1.0, 0.0, 0.0, 0.0],
+            [0.0, 1.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 1.0],
+        ]
+    )
+    source.transform(trans_init)
+    draw_registration_result(source, target, numpy.identity(4))
+
+    source_down, source_fpfh = preprocess_point_cloud(source, voxel_size)
+    target_down, target_fpfh = preprocess_point_cloud(target, voxel_size)
+    return source, target, source_down, target_down, source_fpfh, target_fpfh
+
+
+def execute_global_registration(
+    source_down, target_down, source_fpfh, target_fpfh, voxel_size
+):
+    distance_threshold = voxel_size * 1.5
+    print(":: RANSAC registration on downsampled point clouds.")
+    print("   Since the downsampling voxel size is %.3f," % voxel_size)
+    print("   we use a liberal distance threshold %.3f." % distance_threshold)
+    result = o3d.pipelines.registration.registration_ransac_based_on_feature_matching(
+        source_down,
+        target_down,
+        source_fpfh,
+        target_fpfh,
+        distance_threshold,
+        o3d.pipelines.registration.TransformationEstimationPointToPoint(False),
+        4,
+        [
+            o3d.pipelines.registration.CorrespondenceCheckerBasedOnEdgeLength(0.9),
+            o3d.pipelines.registration.CorrespondenceCheckerBasedOnDistance(
+                distance_threshold
+            ),
+        ],
+        o3d.pipelines.registration.RANSACConvergenceCriteria(4000000, 500),
+    )
+    return result
+
+
+def execute_fast_global_registration(
+    source_down, target_down, source_fpfh, target_fpfh, voxel_size
+):
+    distance_threshold = voxel_size * 0.5
+    print(
+        ":: Apply fast global registration with distance threshold %.3f"
+        % distance_threshold
+    )
+    result = o3d.pipelines.registration.registration_fgr_based_on_feature_matching(
+        source_down,
+        target_down,
+        source_fpfh,
+        target_fpfh,
+        o3d.pipelines.registration.FastGlobalRegistrationOption(
+            maximum_correspondence_distance=distance_threshold
+        ),
+    )
+    return result
+
+
+def start_fast_global_registration():
+    """
+    The function to call from outer scope to start the fast global registration in this file
+    :return:
+    """
+    voxel_size = 0.5  # means 5cm for this dataset
+    (
+        source,
+        target,
+        source_down,
+        target_down,
+        source_fpfh,
+        target_fpfh,
+    ) = prepare_dataset(voxel_size)
+
+    result_fast = execute_fast_global_registration(
+        source_down, target_down, source_fpfh, target_fpfh, voxel_size
+    )
+    print(result_fast)
+    draw_registration_result(source_down, target_down, result_fast.transformation)
--- registration/main.py
+++ registration/main.py
@@ -8,6 +8,8 @@ __copyright__ = "Copyright 2022, Gregor Stief and Optonics"
 __email__ = "Gregormax.Stief@web.de"
 __status__ = "Work in Progress"
 
+import open3d as o3d
+import copy
 import numpy
 from open3d_tasks import (
     create_virtual_object,
@@ -16,13 +18,10 @@ from open3d_tasks import (
     split_point_cloud_by_clusters,
 )
 from util_functions import read_pcd_file, save_pcd_file
+from fast_global_registration import start_fast_global_registration
 
 # create_virtual_object(object_file="data/14_rampspheres.json", save_file="data/14_rampsheres.pcd", save_raw=True)
-point_cloud = read_pcd_file("data/14_rampsheres_clustered.pcd", visualize=False)
+# point_cloud = read_pcd_file("data/14_rampsheres_clustered.pcd", visualize=False)
+# sorted_clusters = split_point_cloud_by_clusters("data/14_rampsheres_clustered.pcd")
 
-sorted_clusters = split_point_cloud_by_clusters(
-    "data/14_rampsheres_clustered.pcd", "data/14_ramsphere"
-)
-
-for item in sorted_clusters:
-    print(len(item.points))
+start_fast_global_registration()
