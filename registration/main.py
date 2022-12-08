ef52c916c0fe1eb4a6ff971cc56021ade545ce63
Gregor <Gregormax.Stief@web.de>
2022-12-08 09:20:19 +0100

    save - because of the cluster trial

--- registration/main.py
+++ registration/main.py
@@ -11,7 +11,29 @@ __status__ = "Work in Progress"
 
 from open3d_tasks import create_virtual_object, cluster_objects
 from util_functions import read_pcd_file
+import time
 
 # create_virtual_object(save_file="data/t_piece_raw.pcd")
-point_cloud = read_pcd_file("data/t_piece_raw.pcd", visualize=False)
-cluster_objects(point_cloud)
+point_cloud = read_pcd_file("data/box_3_piece.pcd", visualize=False)
+
+different_eps = [0.01, 0.1, 0.5, 1, 1.5, 2.0, 2.5, 3.0, 4.0, 5.0]
+different_min_sizes = [*range(10, 110, 10)]
+
+results = []
+
+for eps in different_eps:
+    for size in different_min_sizes:
+        start = time.time()
+        clusters = cluster_objects(
+            point_cloud, eps=eps, min_size=size, print_progress=False
+        )
+        end = time.time()
+        elapsed_time = end - start
+
+        results.append(
+            f"{str(eps):<8} {str(size):<8} {str(elapsed_time):<8.4} {str(clusters):<8}"
+        )
+
+print("eps   Min Size   time    clusters")
+for item in results:
+    print(item)
--- registration/open3d_tasks.py
+++ registration/open3d_tasks.py
@@ -102,7 +102,9 @@ def create_virtual_object(
             o3d.io.write_point_cloud(save_file, cropped_pcd)
 
 
-def cluster_objects(point_cloud):
+def cluster_objects(
+    point_cloud, eps=2, min_size=50, print_progress=False, visualize=False
+):
     """
     Function, that takes a point_cloud and clusters it into elements.
     :param point_cloud:
@@ -110,18 +112,23 @@ def cluster_objects(point_cloud):
     """
     with o3d.utility.VerbosityContextManager(o3d.utility.VerbosityLevel.Debug):
         labels = np.array(
-            point_cloud.cluster_dbscan(eps=2, min_points=50, print_progress=True)
+            point_cloud.cluster_dbscan(
+                eps=eps, min_points=min_size, print_progress=False
+            )
         )
 
     max_label = labels.max()
-    print(f"point cloud has {max_label + 1} clusters")
+    if print_progress:
+        print(f"point cloud has {max_label + 1} clusters")
     colors = plt.get_cmap("tab20")(labels / (max_label if max_label > 0 else 1))
     colors[labels < 0] = 0
     point_cloud.colors = o3d.utility.Vector3dVector(colors[:, :3])
-    o3d.visualization.draw_geometries(
-        [point_cloud],
-        zoom=0.455,
-        front=[-0.4999, -0.1659, -0.8499],
-        lookat=[2.1813, 2.0619, 2.0999],
-        up=[0.1204, -0.9852, 0.1215],
-    )
+    if visualize:
+        o3d.visualization.draw_geometries(
+            [point_cloud],
+            zoom=0.455,
+            front=[-0.4999, -0.1659, -0.8499],
+            lookat=[2.1813, 2.0619, 2.0999],
+            up=[0.1204, -0.9852, 0.1215],
+        )
+    return max_label + 1
