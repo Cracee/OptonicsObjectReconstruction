559287e6d9ca4acdbb0093d986f64f169b64f036
Gregor <Gregormax.Stief@web.de>
2022-12-13 08:42:34 +0100

    deleting a momentarily deadend

--- registration/pyransac_additions.py
+++
@@ -1,7 +0,0 @@
-import pyransac3d as pyrsc
-
-
-points = []  # load_points(.) # Load your point cloud as a numpy array (N, 3)
-
-plane1 = pyrsc.Plane()
-best_eq, best_inliers = plane1.fit(points, 0.01)
